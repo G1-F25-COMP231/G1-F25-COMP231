9bf90f3b3c89ade9e1e67f17a1da4516a5f51cd3
Dsaragil148755 <dsaragil@my.centennialcollege.ca>
2025-12-01 10:39:19 -0500

    Financial Vulnerable Settings UI

---
+++ static/vulnerable-settings.js
@@ -0,0 +1,7 @@
+// Sidebar toggle
+const btn = document.getElementById("toggleSidebar");
+const sidebar = document.getElementById("sidebar");
+
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("hidden");
+});
